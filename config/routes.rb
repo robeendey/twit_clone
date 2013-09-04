667fe02b84a1bd80965d79d7c69de35d914f71fd
Robeen Dey <robeen@djcoursesonline.com>
2013-09-04 12:56:53 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Twitter::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
+  
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
-  get "static_pages/help"
+  resources :users
 
-  get "static_pages/about"
-
-  get "static_pages/contact"
-
+  match '/signup', to: 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
