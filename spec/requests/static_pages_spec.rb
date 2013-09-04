8957de8f8c4f8a26dac99d4e5d2e52e3c4636d0b
Robeen Dey <robeen@djcoursesonline.com>
2013-09-03 22:22:38 -0400

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,20 +6,25 @@ describe "Static pages" do
 
     it "should have the content 'Twitta'" do
       visit '/static_pages/home'
-      page.should have_content('Twitta')
+      page.should have_selector('h1', text: 'Twitta')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', text: "Twitta | Home")
+      page.should have_selector('title', text: "Twitta")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', text: " | Home")
     end
   end
 
   describe "Help page" do
 
-    it "should have the content 'Twitta'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Twitta')
+      page.should have_selector('h1', text: 'Help')
     end
 
     it "should have the right title" do
@@ -30,14 +35,27 @@ describe "Static pages" do
 
   describe "About page" do
 
-    it "should have the content 'Twitta'" do
+    it "should have the h1 'About'" do
       visit '/static_pages/about'
-      page.should have_content('Twitta')
+      page.should have_selector('h1', text: "About")
     end
 
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title', text: "Twitta | About")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit "/static_pages/contact"
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the right title" do
+      visit "/static_pages/contact"
+      page.should have_selector('title', text: 'Twitta | Contact')
     end
   end
 end
